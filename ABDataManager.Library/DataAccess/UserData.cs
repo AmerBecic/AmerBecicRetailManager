806f828328d15c918ab8ea64eed6140c64001c1b
Amer Becic <amer.becic999@gmail.com>
2024-01-26 11:19:58 +0100

    Added DataManager Library project | New UserController
    Added UserController, new connectionString to ABMData, SqlDataAccess (UserModel, UserData -  for Loading)

---
+++ ABDataManager.Library/DataAccess/UserData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ABDataManager.Library.Internal.DataAccess;
+using ABDataManager.Library.Models;
+
+namespace ABDataManager.Library.DataAccess
+{
+    public class UserData
+    {
+        public List<UserModel> GetUserById(string Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var p = new { Id = Id };
+
+           var output = sql.LoadData<UserModel, dynamic>("dbo.spUserLookup", p, "ABMData");
+
+            return output;
+        }
+    }
+}
---
+++ ABDataManager/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using ABDataManager.Library.DataAccess;
+using ABDataManager.Library.Models;
+using Microsoft.AspNet.Identity;
+
+namespace ABDataManager.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        public List<UserModel> GetById()
+        {
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            UserData data = new UserData();
+
+           return data.GetUserById(userId);
+
+        }
+    }
+}
