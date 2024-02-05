fd8b44e7cd471866c9402a7e0329a62ee4da864f
Amer Becic <amer.becic999@gmail.com>
2024-02-05 20:08:16 +0100

    Header LogOut option added

--- ABMDesktopUI.Library/Models/LoggedInUserModel.cs
+++ ABMDesktopUI.Library/Models/LoggedInUserModel.cs
@@ -14,5 +14,15 @@ namespace ABMDesktopUI.Library.Models
         public string LastName { get; set; }
         public string EmailAdress { get; set; }
         public DateTime CreatedDate { get; set; }
+
+        public void LogOffUser()
+        {
+            Token = "";
+            Id = "";
+            FirstName = "";
+            LastName = "";
+            EmailAdress = "";
+            CreatedDate = DateTime.MinValue;
+        }
     }
 }
--- ABMDesktopUI/ViewModels/ShellViewModel.cs
+++ ABMDesktopUI/ViewModels/ShellViewModel.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ABMDesktopUI.EventModels;
+using ABMDesktopUI.Library.Models;
 using Caliburn.Micro;
 
 namespace ABMDesktopUI.ViewModels
@@ -12,14 +13,30 @@ namespace ABMDesktopUI.ViewModels
     {
         private IEventAggregator _events;
         private SalesViewModel _salesVM;
-        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM)
+        private ILoggedInUserModel _user;
+        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM, ILoggedInUserModel user)
         {
             _events = events;
             _salesVM = salesVM;
+            _user = user;
 
             _events.Subscribe(this);
 
             ActivateItem(IoC.Get<LoginViewModel>());
+        }
+
+        public bool IsUserLoggedIn
+        {
+            get
+            {
+                bool output = false;
+
+                if (!string.IsNullOrWhiteSpace(_user.Token))
+                {
+                    output = true;
+                }
+                return output;
+            }
         }
 
         public void ExitApplication()
@@ -27,9 +44,16 @@ namespace ABMDesktopUI.ViewModels
             TryClose();
         }
 
+        public void LogOut()
+        {
+            _user.LogOffUser();
+            ActivateItem(IoC.Get<LoginViewModel>());
+            NotifyOfPropertyChange(() => IsUserLoggedIn);
+        }
         public void Handle(LogOnEvent message)
         {
             ActivateItem(_salesVM);
+            NotifyOfPropertyChange(() => IsUserLoggedIn);
         }
     }
 }
