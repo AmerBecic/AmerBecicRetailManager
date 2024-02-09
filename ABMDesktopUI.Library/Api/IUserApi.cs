388faccf98e8051cf70060b1e87a82742ff1f548
Amer Becic <amer.becic999@gmail.com>
2024-02-09 15:08:04 +0100

    Add and Remove Role from User: Getting info from API to WPF

--- ABMDesktopUI.Library/Api/IUserApi.cs
+++ ABMDesktopUI.Library/Api/IUserApi.cs
@@ -7,5 +7,11 @@ namespace ABMDesktopUI.Library.Api
     public interface IUserApi
     {
         Task<List<ApplicationUserModel>> GetAll();
+        Task<Dictionary<string, string>> GetAllRoles();
+        Task AddRoleToUser(string userId, string roleName);
+        Task RemoveRoleFromUser(string userId, string roleName);
+
+
+
     }
 }
